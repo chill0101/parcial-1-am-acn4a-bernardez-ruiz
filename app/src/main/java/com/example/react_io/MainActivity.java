d8183977c8b9d64ea110fc7d97affe9d6f94968c
Lucas Bernardez <genomarpg@gmail.com>
2025-09-13 15:22:00 -0300

    Start Project commit

---
+++ app/src/main/java/com/example/react_io/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.react_io;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn = findViewById(R.id.btnEmpezar);
+        btn.setOnClickListener(v ->
+                Toast.makeText(this, "Listo para jugar 😎", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
